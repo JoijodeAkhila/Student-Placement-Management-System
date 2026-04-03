dadb3aba9fde42368e7cfa09bc8f9dcaa6a8ec1a
Joijode Akhila <139262763+JoijodeAkhila@users.noreply.github.com>
2026-04-03 11:50:18 +0530

    Add requiredSkill attribute to Company class
    Updated the Company class to include a requiredSkill attribute and modified the constructor and toString method accordingly.

--- Student Placement Management System/model/Company.java
+++ Student Placement Management System/model/Company.java
@@ -3,10 +3,12 @@ package model;
 public class Company {
     private String name;
     private double minCgpa;
+    private String requiredSkill;
 
-    public Company(String name, double minCgpa) {
+    public Company(String name, double minCgpa, String requiredSkill) {
         this.name = name;
         this.minCgpa = minCgpa;
+        this.requiredSkill = requiredSkill;
     }
 
     public String getName() {
@@ -17,8 +19,14 @@ public class Company {
         return minCgpa;
     }
 
+    public String getRequiredSkill() {
+        return requiredSkill;
+    }
+
     @Override
     public String toString() {
-        return "Company: " + name + ", Min CGPA Required: " + minCgpa;
+        return "Company: " + name +
+                ", Min CGPA: " + minCgpa +
+                ", Skill Required: " + requiredSkill;
     }
 }
